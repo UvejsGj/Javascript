ff8a4ed907452b77aab3f3e335f8494a0a8cd633
Uvejs Gjelaj <shiq.une1@gmail.com>
2023-07-06 19:07:49 +0200

    Commit

---
+++ Java e 1/script.js
@@ -0,0 +1,10 @@
+var name = "Uvejs";
+        name = "Rimi ";
+        let lastname = "Gjelaj";
+        const city = "Peje";
+        const age = 16;
+
+        console.log(name)
+        console.log(lastname)
+        console.log(age)
+        window.alert('hhh')
---
+++ Java e 1/script2.js
@@ -0,0 +1,8 @@
+let day = "5th";
+let month = "July";
+let year = 2023
+
+console.log(day)
+console.log(month)
+console.log(year)
+alert("It's Wednesday")
