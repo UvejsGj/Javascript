443d99d7958cc7467e0205d24e69cd16b3f4dc9f
Uvejs Gjelaj <shiq.une1@gmail.com>
2023-07-12 18:39:03 +0200

    Java e 3

---
+++ Java e 2/script.js
@@ -0,0 +1,19 @@
+// let a;
+// let b;
+// let c;
+
+
+
+// a = 10;
+// b = 20;
+// c = 30;
+
+// console.logp(b)
+
+const name = "Simba";
+
+const age = 10;
+
+const sentence = `My cat ${name} is ${age*4+17} years old in cat years.`;
+
+console.log(sentence)
---
+++ Java e 3/script.js
@@ -0,0 +1,6 @@
+const num1 = 5;
+const num2 = 7;
+
+const result = `Adding ${num1} and ${num2} gives ${num1+num2}`;
+
+console.log(result)
